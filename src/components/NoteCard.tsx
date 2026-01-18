14566fbca7e222058670cc3ba81ebe5ff08e4bc6
Negar <negarbaharmand@outlook.com>
2026-01-18 14:55:53 +0100

    Add NoteCard component for displaying notes

---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,13 @@
+import { type Note } from "../types";
+
+interface NoteCardProps {
+  note: Note;
+}
+
+function NoteCard(note: Note) {
+  return (
+    <div>NoteCard</div>
+  )
+}
+
+export default NoteCard
